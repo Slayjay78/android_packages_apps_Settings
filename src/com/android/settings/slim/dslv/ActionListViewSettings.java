173b9fb72a103853d1cd0dc92801b36674bd29ee
Rusty <rusty@Ishigh.com>
2014-12-09 21:10:46 -0800

    Call to the helpers in Velocity

--- src/com/android/settings/slim/dslv/ActionListViewSettings.java
+++ src/com/android/settings/slim/dslv/ActionListViewSettings.java
@@ -53,12 +53,12 @@ import android.widget.TextView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.android.internal.util.slim.ActionConfig;
-import com.android.internal.util.slim.ActionConstants;
-import com.android.internal.util.slim.ActionHelper;
-import com.android.internal.util.slim.ImageHelper;
-import com.android.internal.util.slim.DeviceUtils;
-import com.android.internal.util.slim.DeviceUtils.FilteredDeviceFeaturesArray;
+import com.android.internal.util.velocity.ActionConfig;
+import com.android.internal.util.velocity.ActionConstants;
+import com.android.internal.util.velocity.ActionHelper;
+import com.android.internal.util.velocity.ImageHelper;
+import com.android.internal.util.velocity.DeviceUtils;
+import com.android.internal.util.velocity.DeviceUtils.FilteredDeviceFeaturesArray;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
